49207a1bb44347af65023f7148956b80c96b0921
Diogo Paiva <info@diogopaiva.com>
2022-04-08 01:13:43 +0100

    Setting started -Added Main page structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue'
+import MainBody from './components/MainBody.vue'
+import MainFooter from './components/MainFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainHeader,
+    MainBody,
+    MainFooter
   }
 }
 </script>
+
+<template>
+<div>
+  <MainHeader />
+  <MainBody />
+  <MainFooter />
+</div>
+</template>
+
 
 <style>
 #app {
